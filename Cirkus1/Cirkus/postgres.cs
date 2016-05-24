cc1b8fe1bdfbe18ef4b925e8ff4d411aa022e0a9
ahakille <gorlingy@hotmail.com>
2016-05-24 10:11:28 +0200

    Uppdaterat mera

--- Cirkus1/Cirkus/postgres.cs
+++ Cirkus1/Cirkus/postgres.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Npgsql;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,15 +8,39 @@ using System.Threading.Tasks;
 
 namespace Cirkus
 {
-    class postgres
+    public class postgres
     {
         private NpgsqlConnection _conn;
+        private NpgsqlCommand _cmd;
+        private NpgsqlDataReader _dr;
 
         public Postgres()
         {
-            // _conn= new NpgsqlConnection(ConfigurationManager.connectionstring)
+            // hej=vårt databas
+            _conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["hej"].ConnectionString);
+            _conn.Open();
+            
+        }
+        private NpgsqlDataReader sqlFraga (string sql)
+        {
+            try
+            {
+                _cmd = new NpgsqlCommand(sql, _conn);
+                _dr = _cmd.ExecuteReader();
+                return _dr;
+            }
+            catch (NpgsqlException ex)
+            {
+                //ska försöka bygga klart när erik visar
+                return null;
+            }
+            finally
+            {
+                _conn.Close;
+            }
+                
+            
 
         }
-
     }
 }
