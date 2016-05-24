960b3b3927d59c7c8849882d0e6a020fd9fccc29
ahakille <gorlingy@hotmail.com>
2016-05-24 09:38:50 +0200

    Postgress uppdatering

---
+++ Cirkus1/Cirkus/postgres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cirkus
+{
+    class postgres
+    {
+        private NpgsqlConnection _conn;
+
+        public Postgres()
+        {
+            // _conn= new NpgsqlConnection(ConfigurationManager.connectionstring)
+
+        }
+
+    }
+}
