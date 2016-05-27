66bc010f9b9fd6d689e5dfadd8d84f4c76c9b6d2
ahakille <gorlingy@hotmail.com>
2016-05-27 15:27:39 +0200

    Skapat class medlem med proteys

---
+++ Cirkus1/Cirkus/medlem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cirkus
+{
+    class medlem
+    {
+        public int medlemnr { get; set; }
+        public string förnamn { get; set; }
+        public string efternamn { get; set; }
+        public int födelsedata { get; set; }
+        public string kön { get; set; }
+        public string telefon { get; set; }
+        public string mobilnr { get; set; }
+        public string email { get; set; }
+        public string gata { get; set; }
+        public string postnr { get; set; }
+        public string ort { get; set; }
+        public string medlemstyp { get; set; }
+        public bool foto { get; set; }
+
+    }
+}
