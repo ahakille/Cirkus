bfacbae97f3acd0c3ccbfe777a572e7bf4cf6e16
Nicklas Persson <gorlingy@hotmail.com>
2016-06-01 09:05:28 +0200

    uppdatering

---
+++ Cirkus1/Cirkus/Cirkusupdatemedlem.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Cirkus
+{
+    partial class Cirkusupdatemedlem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.abortBt = new System.Windows.Forms.Button();
+            this.fotoCbox = new System.Windows.Forms.CheckBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.MedlemstypCbox = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.ortTxt = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.postTxt = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.gataTxt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.emailTxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.mobilTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.könCbox = new System.Windows.Forms.ComboBox();
+            this.telefonTxt = new System.Windows.Forms.TextBox();
+            this.enamnTxt = new System.Windows.Forms.TextBox();
+            this.födelsedataTxt = new System.Windows.Forms.TextBox();
+            this.fnamnTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.updateBt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // abortBt
+            // 
+            this.abortBt.Location = new System.Drawing.Point(155, 344);
+            this.abortBt.Name = "abortBt";
+            this.abortBt.Size = new System.Drawing.Size(75, 23);
+            this.abortBt.TabIndex = 51;
+            this.abortBt.Text = "Avbryt";
+            this.abortBt.UseVisualStyleBackColor = true;
+            this.abortBt.Click += new System.EventHandler(this.abortBt_Click);
+            // 
+            // fotoCbox
+            // 
+            this.fotoCbox.AutoSize = true;
+            this.fotoCbox.Location = new System.Drawing.Point(186, 214);
+            this.fotoCbox.Name = "fotoCbox";
+            this.fotoCbox.Size = new System.Drawing.Size(37, 17);
+            this.fotoCbox.TabIndex = 50;
+            this.fotoCbox.Text = "Ja";
+            this.fotoCbox.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(183, 198);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(69, 13);
+            this.label12.TabIndex = 49;
+            this.label12.Text = "Fotografering";
+            // 
+            // MedlemstypCbox
+            // 
+            this.MedlemstypCbox.FormattingEnabled = true;
+            this.MedlemstypCbox.Items.AddRange(new object[] {
+            "Cikrus vän",
+            "Medlem",
+            "Prova på"});
+            this.MedlemstypCbox.Location = new System.Drawing.Point(186, 160);
+            this.MedlemstypCbox.Name = "MedlemstypCbox";
+            this.MedlemstypCbox.Size = new System.Drawing.Size(121, 21);
+            this.MedlemstypCbox.TabIndex = 48;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(183, 143);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(63, 13);
+            this.label11.TabIndex = 47;
+            this.label11.Text = "Medlemstyp";
+            // 
+            // ortTxt
+            // 
+            this.ortTxt.Location = new System.Drawing.Point(183, 116);
+            this.ortTxt.Name = "ortTxt";
+            this.ortTxt.Size = new System.Drawing.Size(100, 20);
+            this.ortTxt.TabIndex = 46;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(183, 103);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 13);
+            this.label10.TabIndex = 45;
+            this.label10.Text = "Ort";
+            // 
+            // postTxt
+            // 
+            this.postTxt.Location = new System.Drawing.Point(183, 76);
+            this.postTxt.Name = "postTxt";
+            this.postTxt.Size = new System.Drawing.Size(100, 20);
+            this.postTxt.TabIndex = 44;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(180, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 13);
+            this.label9.TabIndex = 43;
+            this.label9.Text = "Postnummer";
+            // 
+            // gataTxt
+            // 
+            this.gataTxt.Location = new System.Drawing.Point(180, 37);
+            this.gataTxt.Name = "gataTxt";
+            this.gataTxt.Size = new System.Drawing.Size(100, 20);
+            this.gataTxt.TabIndex = 42;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(177, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "Gata";
+            // 
+            // emailTxt
+            // 
+            this.emailTxt.Location = new System.Drawing.Point(31, 286);
+            this.emailTxt.Name = "emailTxt";
+            this.emailTxt.Size = new System.Drawing.Size(100, 20);
+            this.emailTxt.TabIndex = 40;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 269);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "e-mail";
+            // 
+            // mobilTxt
+            // 
+            this.mobilTxt.Location = new System.Drawing.Point(32, 242);
+            this.mobilTxt.Name = "mobilTxt";
+            this.mobilTxt.Size = new System.Drawing.Size(100, 20);
+            this.mobilTxt.TabIndex = 38;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "Mobilnummer";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Telefonnummer";
+            // 
+            // könCbox
+            // 
+            this.könCbox.FormattingEnabled = true;
+            this.könCbox.Items.AddRange(new object[] {
+            "Kvinna",
+            "Man"});
+            this.könCbox.Location = new System.Drawing.Point(28, 154);
+            this.könCbox.Name = "könCbox";
+            this.könCbox.Size = new System.Drawing.Size(121, 21);
+            this.könCbox.TabIndex = 35;
+            // 
+            // telefonTxt
+            // 
+            this.telefonTxt.Location = new System.Drawing.Point(28, 198);
+            this.telefonTxt.Name = "telefonTxt";
+            this.telefonTxt.Size = new System.Drawing.Size(100, 20);
+            this.telefonTxt.TabIndex = 34;
+            // 
+            // enamnTxt
+            // 
+            this.enamnTxt.Location = new System.Drawing.Point(29, 77);
+            this.enamnTxt.Name = "enamnTxt";
+            this.enamnTxt.Size = new System.Drawing.Size(100, 20);
+            this.enamnTxt.TabIndex = 33;
+            // 
+            // födelsedataTxt
+            // 
+            this.födelsedataTxt.Location = new System.Drawing.Point(28, 116);
+            this.födelsedataTxt.Name = "födelsedataTxt";
+            this.födelsedataTxt.Size = new System.Drawing.Size(100, 20);
+            this.födelsedataTxt.TabIndex = 32;
+            // 
+            // fnamnTxt
+            // 
+            this.fnamnTxt.Location = new System.Drawing.Point(28, 38);
+            this.fnamnTxt.Name = "fnamnTxt";
+            this.fnamnTxt.Size = new System.Drawing.Size(100, 20);
+            this.fnamnTxt.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Kön";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "födelsedata";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Efternamn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Förnman";
+            // 
+            // updateBt
+            // 
+            this.updateBt.Location = new System.Drawing.Point(28, 344);
+            this.updateBt.Name = "updateBt";
+            this.updateBt.Size = new System.Drawing.Size(104, 23);
+            this.updateBt.TabIndex = 26;
+            this.updateBt.Text = "Uppdatera medlem";
+            this.updateBt.UseVisualStyleBackColor = true;
+            this.updateBt.Click += new System.EventHandler(this.updateBt_Click);
+            // 
+            // Cirkusupdatemedlem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 433);
+            this.Controls.Add(this.abortBt);
+            this.Controls.Add(this.fotoCbox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.MedlemstypCbox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.ortTxt);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.postTxt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.gataTxt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.emailTxt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.mobilTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.könCbox);
+            this.Controls.Add(this.telefonTxt);
+            this.Controls.Add(this.enamnTxt);
+            this.Controls.Add(this.födelsedataTxt);
+            this.Controls.Add(this.fnamnTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.updateBt);
+            this.Name = "Cirkusupdatemedlem";
+            this.Text = "Cirkusupdatemedlem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button abortBt;
+        private System.Windows.Forms.CheckBox fotoCbox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox MedlemstypCbox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox ortTxt;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox postTxt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox gataTxt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox emailTxt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox mobilTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox könCbox;
+        private System.Windows.Forms.TextBox telefonTxt;
+        private System.Windows.Forms.TextBox enamnTxt;
+        private System.Windows.Forms.TextBox födelsedataTxt;
+        private System.Windows.Forms.TextBox fnamnTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button updateBt;
+    }
+}
