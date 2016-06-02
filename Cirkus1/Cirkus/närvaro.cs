8a557074906ade340ee687a93b3cc4bdc831521a
Christoffer Karlsson <chka1207@student.miun.se>
2016-06-02 19:37:28 +0200

    Närvaroklassen

--- Cirkus1/Cirkus/närvaro.cs
+++ Cirkus1/Cirkus/närvaro.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Cirkus
 {
-    class närvaro : medlem  
+    public class närvaro : medlem  
     {
+        public int GruppID { get; set; }
+
+        public void GetGrupptillfälle(int gruppID)
+        {
+            GruppID = gruppID;
+
+            postgres n = new postgres();
+            n.SqlAdmin("select g.namn,t.datum, t.tid, a.aktivitet, t.id from träningstillfälle t, deltar d, träningsgrupp g, träningstyp a where g.gruppid = " + 6 + " and d.träningsgrupp = g.gruppid and t.id = d.träningstillfalle and a.id = t.aktivtetsid; ");
+        }
+
+        public override string ToString()
+        {
+            return " " + Förnamn + " " + Efternamn + ", " + Födelsedata;
+        }
     }
 }
