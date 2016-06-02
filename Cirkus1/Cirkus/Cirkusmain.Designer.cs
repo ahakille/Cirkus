af5317a62c9591c11c054880a53c9d75c869f971
ahakille <gorlingy@hotmail.com>
2016-06-03 01:00:09 +0200

    Ny desin första sidan

--- Cirkus1/Cirkus/Cirkusmain.Designer.cs
+++ Cirkus1/Cirkus/Cirkusmain.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.träningsgruppBT = new System.Windows.Forms.Button();
             this.narvaroBt = new System.Windows.Forms.Button();
             this.updateMedlemBt = new System.Windows.Forms.Button();
             this.tillfälleBox = new System.Windows.Forms.ListBox();
             this.NyttTräningstillfälleBt = new System.Windows.Forms.Button();
             this.LbMedlem = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -48,20 +48,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(33, 207);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Uppdatera";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // träningsgruppBT
             // 
-            this.träningsgruppBT.Location = new System.Drawing.Point(562, 113);
+            this.träningsgruppBT.Location = new System.Drawing.Point(221, 60);
             this.träningsgruppBT.Name = "träningsgruppBT";
-            this.träningsgruppBT.Size = new System.Drawing.Size(146, 53);
+            this.träningsgruppBT.Size = new System.Drawing.Size(145, 67);
             this.träningsgruppBT.TabIndex = 8;
             this.träningsgruppBT.Text = "Hantera Träningsgrupper";
             this.träningsgruppBT.UseVisualStyleBackColor = true;
@@ -69,7 +60,7 @@
             // 
             // narvaroBt
             // 
-            this.narvaroBt.Location = new System.Drawing.Point(562, 323);
+            this.narvaroBt.Location = new System.Drawing.Point(221, 156);
             this.narvaroBt.Name = "narvaroBt";
             this.narvaroBt.Size = new System.Drawing.Size(145, 70);
             this.narvaroBt.TabIndex = 9;
@@ -79,9 +70,9 @@
             // 
             // updateMedlemBt
             // 
-            this.updateMedlemBt.Location = new System.Drawing.Point(562, 38);
+            this.updateMedlemBt.Location = new System.Drawing.Point(46, 60);
             this.updateMedlemBt.Name = "updateMedlemBt";
-            this.updateMedlemBt.Size = new System.Drawing.Size(145, 53);
+            this.updateMedlemBt.Size = new System.Drawing.Size(145, 67);
             this.updateMedlemBt.TabIndex = 10;
             this.updateMedlemBt.Text = "Hantera medlemar";
             this.updateMedlemBt.UseVisualStyleBackColor = true;
@@ -90,16 +81,16 @@
             // tillfälleBox
             // 
             this.tillfälleBox.FormattingEnabled = true;
-            this.tillfälleBox.Location = new System.Drawing.Point(36, 246);
+            this.tillfälleBox.Location = new System.Drawing.Point(36, 298);
             this.tillfälleBox.Name = "tillfälleBox";
-            this.tillfälleBox.Size = new System.Drawing.Size(482, 147);
+            this.tillfälleBox.Size = new System.Drawing.Size(347, 95);
             this.tillfälleBox.TabIndex = 12;
             // 
             // NyttTräningstillfälleBt
             // 
-            this.NyttTräningstillfälleBt.Location = new System.Drawing.Point(562, 246);
+            this.NyttTräningstillfälleBt.Location = new System.Drawing.Point(45, 156);
             this.NyttTräningstillfälleBt.Name = "NyttTräningstillfälleBt";
-            this.NyttTräningstillfälleBt.Size = new System.Drawing.Size(146, 53);
+            this.NyttTräningstillfälleBt.Size = new System.Drawing.Size(146, 70);
             this.NyttTräningstillfälleBt.TabIndex = 13;
             this.NyttTräningstillfälleBt.Text = "Skapa nytt träningstillfälle";
             this.NyttTräningstillfälleBt.UseVisualStyleBackColor = true;
@@ -108,24 +99,34 @@
             // LbMedlem
             // 
             this.LbMedlem.AutoSize = true;
-            this.LbMedlem.Location = new System.Drawing.Point(45, 78);
+            this.LbMedlem.Location = new System.Drawing.Point(33, 282);
             this.LbMedlem.Name = "LbMedlem";
-            this.LbMedlem.Size = new System.Drawing.Size(35, 13);
+            this.LbMedlem.Size = new System.Drawing.Size(129, 13);
             this.LbMedlem.TabIndex = 14;
-            this.LbMedlem.Text = "label1";
+            this.LbMedlem.Text = "Senaste träningstillfällerna";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 24);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Cirkus kul och bus";
             // 
             // Cirkusmain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(733, 453);
+            this.ClientSize = new System.Drawing.Size(428, 453);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.LbMedlem);
             this.Controls.Add(this.NyttTräningstillfälleBt);
             this.Controls.Add(this.tillfälleBox);
             this.Controls.Add(this.updateMedlemBt);
             this.Controls.Add(this.narvaroBt);
             this.Controls.Add(this.träningsgruppBT);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Cirkusmain";
             this.Text = "Cirkus - Kul & Bus  Admin";
@@ -138,13 +139,13 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button träningsgruppBT;
         private System.Windows.Forms.Button narvaroBt;
         private System.Windows.Forms.Button updateMedlemBt;
         private System.Windows.Forms.ListBox tillfälleBox;
         private System.Windows.Forms.Button NyttTräningstillfälleBt;
         private System.Windows.Forms.Label LbMedlem;
+        private System.Windows.Forms.Label label1;
     }
 }
 
