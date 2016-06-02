cacdf2f9a0e781e6ce2f4a12c6be408040ca7b6e
Christoffer Karlsson <chka1207@student.miun.se>
2016-06-02 09:52:12 +0200

    Ny class, ej klar

---
+++ Cirkus1/Cirkus/Ansvar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cirkus
+{
+    public class Ansvar
+    {
+        public int AnsvarsID { get; set; }
+        public string Ansvarsområde { get; set; }
+
+        public void LaggTillAnsvarsområde(string ansvarsområde)
+        {
+            Ansvarsområde = ansvarsområde;
+
+            postgres a = new postgres();
+
+            a.SqlAdmin("insert into ansvarsområde (ansvar) values ('" + ansvarsområde + "');");
+        }
+    }
+}
